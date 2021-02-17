444f1f649759806bbcc6b3532ac2e9254724bd79
Sourav Chatterjee <sromic@gmail.com>
2021-02-17 19:24:55 +0530

    Added building types and building type list

---
+++ Assets/BuilderDefender/_Scripts/BuildingManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    private BuildingTypeSO buildingType;
+    private BuildingTypeListSO buildingTypeList;
+    private Camera mainCamera;
+    
+    void Start()
+    {
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeSO).Name);
+        buildingType = buildingTypeList.list[0];
+        mainCamera = Camera.main;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            buildingType = buildingTypeList.list[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            buildingType = buildingTypeList.list[1];
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
---
+++ Assets/BuilderDefender/_Scripts/BuildingTypeSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/BuildingType")]
+public class BuildingTypeSO : ScriptableObject
+{
+    public string nameString;
+    public Transform prefab;
+}
